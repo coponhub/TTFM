0e8149f1a5c7b34dfb564dbef8168c17de23d711
coponhub <coponhtc@gmail.com>
2026-01-30 21:22:49 +0900

    refactor: overhaul Oneview schema engine with declarative DSL

--- src/oneview.rs
+++ src/oneview.rs
@@ -95,6 +95,7 @@ fn build_rank_expr() -> sea_query::SimpleExpr {
 }
 
 /// item_kind の CASE 式を生成
+/// 注意: これは FileReferences と ItemReferences の両方が JOIN されている環境（Tag Source）でのみ使用可能。
 fn build_item_kind_expr() -> sea_query::SimpleExpr {
     CaseStatement::new()
         .case(
@@ -109,18 +110,147 @@ fn build_item_kind_expr() -> sea_query::SimpleExpr {
         .into()
 }
 
-/// tag (typed_tag) の CONCAT 式を生成
-fn build_tag_expr(
-    tbl: Tbl,
-    label_str_expr: sea_query::SimpleExpr,
-) -> sea_query::SimpleExpr {
+/// Oneview のデータソースを識別するための型
+enum OneViewSource<'a> {
+    /// タグテーブル (Base, System, UserTags)
+    Tag(&'a TagSource),
+    /// 物理テーブル (FileReferences, Locations) の一般カラム
+    Physical { cd: &'a ColumnDef, tbl: Tbl },
+    /// Locations テーブルの name/filename エイリアス
+    LocationAlias(Val),
+    /// ItemReferences (非ファイルアイテム) の unpivot カラム
+    ItemRef(Col),
+}
+
+// ----------------------------------------------------------------------------
+// 各カラムの「仕様（Spec）」を定義する関数群
+// ----------------------------------------------------------------------------
+
+fn spec_origin(source: &OneViewSource) -> sea_query::SimpleExpr {
+    // UserTags の時だけ User、それ以外は System
+    if matches!(source, OneViewSource::Tag(s) if s.table == Tbl::UserTags) {
+        Expr::val(Into::<&'static str>::into(Val::User))
+            .cast_as(SqlType::VARCHAR)
+            .into()
+    } else {
+        Expr::val(Into::<&'static str>::into(Val::System))
+            .cast_as(SqlType::VARCHAR)
+            .into()
+    }
+}
+
+fn spec_rank(source: &OneViewSource) -> sea_query::SimpleExpr {
+    match source {
+        // Tag系は FileRefs と ItemRefs 両方を JOIN しているため、元々の build_rank_expr が使える
+        OneViewSource::Tag(_) => build_rank_expr(),
+
+        // Physical系（FileReferences, Locations）
+        OneViewSource::Physical { tbl, .. } => {
+            if *tbl == Tbl::FileReferences {
+                // FileReferences 自身なら直のカラム
+                Expr::col((*tbl, Col::Rank)).into()
+            } else {
+                // Locations なら FileReferences を JOIN している
+                Func::cust(crate::db::DuckDbFunc::Coalesce)
+                    .args([
+                        Expr::col((Tbl::FileReferences, Col::Rank)).into(),
+                        Expr::val(0).into(),
+                    ])
+                    .into()
+            }
+        }
+
+        // LocationAlias も Locations 同様
+        OneViewSource::LocationAlias(_) => Func::cust(crate::db::DuckDbFunc::Coalesce)
+            .args([
+                Expr::col((Tbl::FileReferences, Col::Rank)).into(),
+                Expr::val(0).into(),
+            ])
+            .into(),
+
+        // ItemRef は自身のテーブルの Rank カラムを直接使う
+        OneViewSource::ItemRef(_) => Expr::col(Col::Rank).into(),
+    }
+}
+
+fn spec_item_kind(source: &OneViewSource) -> sea_query::SimpleExpr {
+    match source {
+        // Tag系は両方の JOIN があるため共通ロジックが使える
+        OneViewSource::Tag(_) => build_item_kind_expr().cast_as(SqlType::VARCHAR).into(),
+
+        // Physical系およびエイリアスは常に File 確定
+        OneViewSource::Physical { .. } | OneViewSource::LocationAlias(_) => {
+            Expr::val(Into::<&'static str>::into(Val::File))
+                .cast_as(SqlType::VARCHAR)
+                .into()
+        }
+
+        // ItemRef は自身のカラムを直接使う
+        OneViewSource::ItemRef(_) => Expr::col(Col::ItemKind).cast_as(SqlType::VARCHAR).into(),
+    }
+}
+
+fn spec_type(source: &OneViewSource) -> sea_query::SimpleExpr {
+    let expr = match source {
+        OneViewSource::Tag(s) => Expr::col((s.table, Col::Type)),
+        OneViewSource::Physical { cd, .. } => Expr::val(&cd.name[..]),
+        OneViewSource::LocationAlias(v) => Expr::val(Into::<&'static str>::into(*v)),
+        OneViewSource::ItemRef(col) => Expr::val(Into::<&'static str>::into(*col)),
+    };
+    expr.cast_as(SqlType::VARCHAR).into()
+}
+
+fn spec_label_str(source: &OneViewSource) -> sea_query::SimpleExpr {
+    let expr: sea_query::SimpleExpr = match source {
+        OneViewSource::Tag(s) => build_label_str_expr(s.table),
+        OneViewSource::Physical { cd, tbl } => {
+            Expr::col((*tbl, crate::util::col_to_iden(&cd.name))).into()
+        }
+        OneViewSource::LocationAlias(_) => Expr::col((Tbl::Locations, Col::Filename)).into(),
+        OneViewSource::ItemRef(col) => Expr::col(*col).into(),
+    };
+    expr.cast_as(SqlType::VARCHAR).into()
+}
+
+fn spec_typed_tag(source: &OneViewSource) -> sea_query::SimpleExpr {
+    // 素材を集める
+    let type_expr = spec_type(source);
+    let val_expr: sea_query::SimpleExpr = match source {
+        OneViewSource::Tag(s) => build_label_str_expr(s.table),
+        OneViewSource::Physical { cd, tbl } => {
+            Expr::col((*tbl, crate::util::col_to_iden(&cd.name))).into()
+        }
+        OneViewSource::LocationAlias(_) => Expr::col((Tbl::Locations, Col::Filename)).into(),
+        OneViewSource::ItemRef(col) => Expr::col(*col).into(),
+    };
+
+    // "type:value" 形式で結合
     Func::cust(crate::db::DuckDbFunc::Concat)
         .args([
-            Expr::col((tbl, Col::Type)).cast_as(SqlType::VARCHAR).into(),
+            type_expr.into(),
             Expr::val(":").into(),
-            label_str_expr.into(),
+            val_expr.cast_as(SqlType::VARCHAR).into(),
         ])
         .into()
+}
+
+/// Oneview の共通カラム群を一括設定する「真の集約エンジン」
+fn apply_oneview_schema(q: &mut sea_query::SelectStatement, source: OneViewSource) {
+    let s = &source;
+    add_col(Col::Origin, spec_origin, q, s);
+    add_col(Col::Rank, spec_rank, q, s);
+    add_col(Col::ItemKind, spec_item_kind, q, s);
+    add_col(Col::Type, spec_type, q, s);
+    add_col(Col::TypedTag, spec_typed_tag, q, s);
+}
+
+fn add_col(
+    col: Col,
+    f: fn(&OneViewSource) -> sea_query::SimpleExpr,
+    q: &mut sea_query::SelectStatement,
+    s: &OneViewSource,
+) {
+    q.expr_as(f(s), col);
 }
 
 /// Locations テーブルの name/filename エイリアス用クエリを生成
@@ -131,45 +261,15 @@ fn build_location_alias_query(
 ) -> String {
     let mut q = Query::select();
     q.column((Tbl::Locations, Col::ItemId))
-        .expr_as(
-            Expr::val(Into::<&'static str>::into(Val::System))
-                .cast_as(SqlType::VARCHAR),
-            Col::Origin,
-        )
-        .expr_as(
-            Func::cust(crate::db::DuckDbFunc::Coalesce).args([
-                Expr::col((Tbl::FileReferences, Col::Rank)).into(),
-                Expr::val(0).into(),
-            ]),
-            Col::Rank,
-        )
-        .expr_as(
-            Expr::val(Into::<&'static str>::into(Val::File))
-                .cast_as(SqlType::VARCHAR),
-            Col::ItemKind,
-        )
-        .expr_as(
-            Expr::val(Into::<&'static str>::into(type_val))
-                .cast_as(SqlType::VARCHAR),
-            Col::Type,
-        )
         .expr_as(Expr::col((Tbl::Locations, Col::Filename)), Col::LabelStr)
         .expr_as(crate::util::null_as(SqlType::BIGINT), Col::LabelInt)
         .expr_as(crate::util::null_as(SqlType::DOUBLE), Col::LabelDouble)
-        .expr_as(crate::util::null_as(SqlType::BOOLEAN), Col::LabelBool)
-        .expr_as(
-            Func::cust(crate::db::DuckDbFunc::Concat).args([
-                Expr::val(Into::<&'static str>::into(type_val))
-                    .cast_as(SqlType::VARCHAR)
-                    .into(),
-                Expr::val(":").into(),
-                Expr::col((Tbl::Locations, Col::Filename))
-                    .cast_as(SqlType::VARCHAR)
-                    .into(),
-            ]),
-            Col::TypedTag,
-        )
-        .from_subquery(crate::util::parquet_query(parquet_path), Tbl::Locations)
+        .expr_as(crate::util::null_as(SqlType::BOOLEAN), Col::LabelBool);
+
+    // 【仕様の完全集約】全共通カラムをエンジンに委託
+    apply_oneview_schema(&mut q, OneViewSource::LocationAlias(type_val));
+
+    q.from_subquery(crate::util::parquet_query(parquet_path), Tbl::Locations)
         .join_subquery(
             sea_query::JoinType::LeftJoin,
             crate::util::parquet_query(file_ref_path),
@@ -227,40 +327,13 @@ fn build_physical_column_query(
     let iden = crate::util::col_to_iden(&cd.name);
 
     let mut q = Query::select();
-    q.column((tbl_alias, Col::ItemId))
-        .expr_as(
-            Expr::val(Into::<&'static str>::into(Val::System))
-                .cast_as(SqlType::VARCHAR),
-            Col::Origin,
-        )
-        .expr_as(
-            Func::cust(crate::db::DuckDbFunc::Coalesce).args([
-                Expr::col((Tbl::FileReferences, Col::Rank)).into(),
-                Expr::val(0).into(),
-            ]),
-            Col::Rank,
-        )
-        .expr_as(
-            Expr::val(Into::<&'static str>::into(Val::File))
-                .cast_as(SqlType::VARCHAR),
-            Col::ItemKind,
-        )
-        .expr_as(Expr::val(&cd.name[..]).cast_as(SqlType::VARCHAR), Col::Type);
+    q.column((tbl_alias, Col::ItemId));
 
     // ラベルカラムの設定（型に応じて分岐）
     apply_label_columns(&mut q, tbl_alias, &iden, cd.sql_type);
 
-    // tag column
-    q.expr_as(
-        Func::cust(crate::db::DuckDbFunc::Concat).args([
-            Expr::val(&cd.name[..]).cast_as(SqlType::VARCHAR).into(),
-            Expr::val(":").into(),
-            Expr::col((tbl_alias, iden))
-                .cast_as(SqlType::VARCHAR)
-                .into(),
-        ]),
-        Col::TypedTag,
-    );
+    // 【仕様の完全集約】全共通カラムをエンジンに委託
+    apply_oneview_schema(&mut q, OneViewSource::Physical { cd, tbl: tbl_alias });
 
     q.from_subquery(crate::util::parquet_query(parquet_path), tbl_alias);
 
@@ -281,34 +354,15 @@ fn build_physical_column_query(
 fn build_item_ref_query(col: Col, items_path: &str) -> String {
     let label_col = Col::from_sql_type(SqlType::VARCHAR);
     let mut q = Query::select();
-    q.column(Col::ItemId)
-        .expr_as(
-            Expr::val(Into::<&'static str>::into(Val::System))
-                .cast_as(SqlType::VARCHAR),
-            Col::Origin,
-        )
-        .expr_as(Expr::col(Col::Rank), Col::Rank)
-        .expr_as(
-            Expr::col(Col::ItemKind).cast_as(SqlType::VARCHAR),
-            Col::ItemKind,
-        )
-        .expr_as(
-            Expr::val::<&str>(col.into()).cast_as(SqlType::VARCHAR),
-            Col::Type,
-        )
-        .expr_as(Expr::col(col), label_col)
-        .expr_as(
-            Func::cust(crate::db::DuckDbFunc::Concat).args([
-                Expr::val::<&str>(col.into()).into(),
-                Expr::val(":").into(),
-                Expr::col(col).into(),
-            ]),
-            Col::TypedTag,
-        )
-        .from_subquery(
-            crate::util::parquet_query(items_path),
-            Tbl::ItemReferences,
-        );
+    q.column(Col::ItemId).expr_as(Expr::col(col), label_col);
+
+    // 【仕様の完全集約】全共通カラムをエンジンに委託
+    apply_oneview_schema(&mut q, OneViewSource::ItemRef(col));
+
+    q.from_subquery(
+        crate::util::parquet_query(items_path),
+        Tbl::ItemReferences,
+    );
     q.to_string(PostgresQueryBuilder)
 }
 
@@ -322,20 +376,15 @@ fn build_tag_query(
 
     let mut q = Query::select();
     q.column((tbl, Col::ItemId))
-        .expr_as(
-            Expr::val(Into::<&'static str>::into(source.origin))
-                .cast_as(SqlType::VARCHAR),
-            Col::Origin,
-        )
-        .expr_as(build_rank_expr(), Col::Rank)
-        .expr_as(build_item_kind_expr(), Col::ItemKind)
-        .column((tbl, Col::Type))
-        .expr_as(build_tag_expr(tbl, label_str.clone()), Col::TypedTag)
         .expr_as(label_str, Col::LabelStr)
         .column((tbl, Col::LabelInt))
         .column((tbl, Col::LabelDouble))
-        .column((tbl, Col::LabelBool))
-        .from_subquery(crate::util::parquet_query(&path_fn(source.target)), tbl)
+        .column((tbl, Col::LabelBool));
+
+    // 【仕様の完全集約】全共通カラムをエンジンに委託
+    apply_oneview_schema(&mut q, OneViewSource::Tag(source));
+
+    q.from_subquery(crate::util::parquet_query(&path_fn(source.target)), tbl)
         .join_subquery(
             sea_query::JoinType::LeftJoin,
             crate::util::parquet_query(&path_fn(TargetTable::FileReferences)),
